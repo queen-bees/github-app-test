206db14733a5509d31788461d3972a9bbbce8c58
Ji Park <jhp0621@gmail.com>
2023-02-22 10:51:35 -0600

    re-write app purpose

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
-<h2>This is to create a pull request to see if the event handler gets triggered!</h2>
+<h2>This app is to create pull requests to test the PR-to-Asana Github App's functionality!</h2>
 <p>Hello World! My name is Ji. I am a software developer.</p>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
