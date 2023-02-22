4dc3a447d962455e4528c20fd926087722304289
Ji Park <jhp0621@gmail.com>
2023-02-22 15:15:09 -0600

    remove last line

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ document.getElementById("app").innerHTML = `
 <h2>This app is to create pull requests to test PR-to-Asana Github App's functionality.</h2>
 <p>My name is Ji Park from Seoul, South Korea. I live in Chicago, IL.</p>
 <p>I am a software developer.</p>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
 `;
