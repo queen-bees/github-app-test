862ee001e5d18a747d07a17b9c1cfaec202fe8a0
Ji Park <jhp0621@gmail.com>
2023-02-22 14:24:09 -0600

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<h2>This app is to create pull requests to test the PR-to-Asana Github App's functionality!</h2>
+<h1>Hello World!</h1>
+<h2>This app is to create pull requests to test PR-to-Asana Github App's functionality.</h2>
 <p>My name is Ji Park from Seoul, South Korea. I live in Chicago, IL.</p>
 <p>I am a software developer.</p>
 <div>
