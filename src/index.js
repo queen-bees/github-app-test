4ee29ccd26634529b5d772ee8d5dc351af5737bc
Ji Park <jhp0621@gmail.com>
2023-02-22 18:54:40 -0600

    add company name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.getElementById("app").innerHTML = `
 <h1>Hello World!</h1>
 <h2>This app is to create pull requests to test PR-to-Asana Github App's functionality.</h2>
 <p>My name is Ji Park from Seoul, South Korea. I live in Chicago, IL.</p>
-<p>I am a software developer.</p>
+<p>I am a software developer. I work at LaunchPad Labs.</p>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
